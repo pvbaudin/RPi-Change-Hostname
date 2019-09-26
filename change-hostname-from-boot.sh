c4ba13aeb62fc9dabb86e384900479354bd876af
Pierre Baudin <pvbaudin@ucsc.edu>
2019-09-26 16:34:33 -0700

    removed dependency of change-hostname.sh for change-hostname-from-boot.sh

--- change-hostname-from-boot.sh
+++ change-hostname-from-boot.sh
@@ -1,5 +1,7 @@
 if [ "$(hostname)" = "$(cat /boot/hostname.txt)" ]; then
 	echo "the names match" 
 else
-	sudo sh /home/pi/scripts/change-hostname.sh $(cat /boot/hostname.txt) 
+	sudo sed -i "s/$(hostname)/$(cat /boot/hostname.txt)/g" /etc/hosts
+	sudo sed -i "s/$(hostname)/$(cat /boot/hostname.txt)/g" /etc/hostname
+	sudo reboot
 fi
